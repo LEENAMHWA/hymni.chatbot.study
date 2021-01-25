366b6e20e8bea75cd551cbe7977e8e53e7769d5b
Minsurr <77606739+Minsurr@users.noreply.github.com>
2021-01-25 16:52:18 +0900

    Update 210125_2016_Minsurr.c

--- Code/210125_2016_Minsurr.c
+++ Code/210125_2016_Minsurr.c
@@ -20,34 +20,31 @@ int main(void) {
 }
 
 char* solution(int a, int b) {
-	
-	// 리턴할 값은 동적메모리로 할당해주세요.
+	// 리턴할 값은 동적메모리로 할당.
 	char* answer = (char*)malloc(7); 
-    /*1월 1일은 금요일, 1월 8일도 금요일
-	  % 7로 연산시 1이 남으면 금요일이기 때문에 배열 index설정 편의를 위해
-	  dayName[] index 1번에 금요일을 배치.
+	/*
+	1월 1일은 금요일, 1월 8일도 금요일
+	% 7로 연산시 1이 남으면 금요일이기 때문에 배열 index설정 편의를 위해
+	dayName[] index 1번에 금요일을 배치.
 	*/
-    char* dayName[] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-    
-    // 각 월에 해당하는 일수를 입력해놓음
-    int day[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    
-    int daySum = 0;
+	char* dayName[] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+	
+	// 각 월에 해당하는 일수를 입력.
+	int day[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int daySum = 0;
 	int i = 0;
-    int check = 0;
+	int check = 0;
     
-	/*인자로 넘겨받는 월에 해당하는 위치 지정
-	 및 해당 월까지의 모든 일의 합.
-	*/ 
-    for(i = 0; i < a - 1; i++) {
-        daySum = daySum +  day[i];
-    }
+	//해당 월 이전 월까지의 일수 합. 
+	for(i = 0; i < a - 1; i++) {
+		daySum = daySum +  day[i];
+    	}
 	
 	// 해당 월 전월까지의 모든일 합 + 해당 월의 일 합
 	daySum = daySum + b;
 	check = daySum % 7;     
-
-    answer = dayName[check];
-    return answer;
+	
+	answer = dayName[check];
+	return answer;
 	free(answer);
 }
